4ffc0f72176a10a9f7df44d08d09071cb3ce2a9c
DavidJPLeRoux <djpleroux@gmail.com>
2023-12-09 00:39:04 -0500

    fbiuabf
    hkjisfbiusdv

--- app/src/main/java/com/example/pantrypal/MainActivity.kt
+++ app/src/main/java/com/example/pantrypal/MainActivity.kt
@@ -23,8 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //PantryPalApp()
-                    TestScreen()
+                    PantryPalApp()
+                    //TestScreen()
                 }
             }
         }
