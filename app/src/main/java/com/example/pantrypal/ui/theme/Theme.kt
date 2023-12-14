5f8df0c6058306a9789e188d1655786665a36ae0
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2023-12-13 21:10:21 -0500

    color change

--- app/src/main/java/com/example/pantrypal/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pantrypal/ui/theme/Theme.kt
@@ -97,39 +97,3 @@ fun PantryPalTheme(
         content = content
     )
 }
-
-/*
-@Composable
-fun PantryPalTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        */
-/*dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }*//*
-
-
-        darkTheme -> DarkColors
-        else -> LightColors
-
-//        else -> LightColors
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
-}*/
